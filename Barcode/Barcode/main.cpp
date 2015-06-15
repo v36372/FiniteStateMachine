326252e881c855c8f5c2c9e08935612952c0b9d2
v36372 <nguyentrongtin2331@gmail.com>
2015-06-15 17:55:49 +0700

    + add automatically translate from graphml to JSON

--- Barcode/Barcode/main.cpp
+++ Barcode/Barcode/main.cpp
@@ -3,6 +3,10 @@
 
 int main()
 {
+	std::stringstream stream;
+	stream << "FSMGraphParser.exe " << "colorchange.graphml";
+	system(stream.str().c_str());
+
 	TestFSM test("colorchange.txt");
 	test.Start();
 	std::string tmp = "";
@@ -10,15 +14,15 @@ int main()
 	{
 		test.Update(30.f);
 		Sleep(2);
-		if (tmp == "")
-		{
-			std::cout << "ban co' muon' raise event OnRed khong ? input yes or no :";
-			std::cin >> tmp;
-			if (tmp == "yes")
-			{
-				test.RaiseEvent("OnRed");
-			}
-		}
+		//if (tmp == "")
+		//{
+			//std::cout << "ban co' muon' raise event OnRed khong ? input yes or no :";
+			//std::cin >> tmp;
+			//if (tmp == "yes")
+			//{
+			//	test.RaiseEvent("OnRed");
+			//}
+		//}
 	}
 	
 	
